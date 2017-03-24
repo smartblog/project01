4e873f581b5e2818686b16bd47b001b87c7b9449
Boris Stukov <stukov.boris@gmail.com>
2017-03-24 15:26:00 +0600

    Lesson06 add route validate

--- lessons06/route.rb
+++ lessons06/route.rb
@@ -1,7 +1,10 @@
 class Route
   attr_accessor :route
   def initialize(from, to)
-    @route = [from, to]
+    @from = from
+    @to = to
+    validate!
+    @route = [@from, @to]
   end
 
   def add_station(station)
@@ -10,5 +13,11 @@ class Route
 
   def delete_station(station)
     @transit.delete(station)
+  end
+
+  def validate!
+    raise "From is not Station" if @from.class != Station
+    raise "To is not Station" if @to.class != Station
+    true
   end
 end
